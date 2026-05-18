b3b70c957979650c17c8b308fcfbd9e359f9384d
virckdev <wellwellwellboyeatingchicken@gmail.com>
2026-05-17 20:51:15 -0500

    Update start.lua

--- start.lua
+++ start.lua
@@ -6,7 +6,7 @@ task.spawn(function()
 		warn("Failed to load Infinite Yield:", err)
 	end
 end)
-wait(0.3)
+
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/main/aesthetic.lua"))()
@@ -15,7 +15,7 @@ task.spawn(function()
 		warn("Failed to load Aesthetic Sky:", err)
 	end
 end)
-wait(0.3)
+
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/refs/heads/main/AestheticWater.lua"))()
@@ -24,7 +24,7 @@ task.spawn(function()
 		warn("Failed to load Aesthetic Water:", err)
 	end
 end)
-wait(0.3)
+
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/refs/heads/main/tele.lua"))()
@@ -33,7 +33,7 @@ task.spawn(function()
 		warn("Failed to load House Teleport Tool:", err)
 	end
 end)
-wait(4)
+wait(2.6)
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/virckdev/jewish/refs/heads/main/ordinary.lua"))()
