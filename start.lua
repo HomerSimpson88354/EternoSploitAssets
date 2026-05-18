2575662ac30cf6d0eafec2b155476c1f994a9527
virckdev <wellwellwellboyeatingchicken@gmail.com>
2026-05-17 20:41:11 -0500

    Adjust wait times and error messages in start.lua

--- start.lua
+++ start.lua
@@ -6,7 +6,7 @@ task.spawn(function()
 		warn("Failed to load Infinite Yield:", err)
 	end
 end)
-
+wait(0.3)
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/main/aesthetic.lua"))()
@@ -15,7 +15,7 @@ task.spawn(function()
 		warn("Failed to load Aesthetic Sky:", err)
 	end
 end)
-
+wait(0.3)
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/refs/heads/main/AestheticWater.lua"))()
@@ -24,7 +24,7 @@ task.spawn(function()
 		warn("Failed to load Aesthetic Water:", err)
 	end
 end)
-
+wait(0.3)
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/refs/heads/main/tele.lua"))()
@@ -33,12 +33,12 @@ task.spawn(function()
 		warn("Failed to load House Teleport Tool:", err)
 	end
 end)
-wait(0.1)
+wait(2.5)
 task.spawn(function()
 	local ok, err = pcall(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/virckdev/jewish/refs/heads/main/ordinary.lua"))()
 	end)
 	if not ok then
-		warn("...", err)
+		warn("failed to load")
 	end
 end)
