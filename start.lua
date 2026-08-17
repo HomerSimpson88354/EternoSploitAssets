a3d9d3694579703b83b2ebd85281d2ca2fb8c8e7
HomerSimpson88354 <182835240+HomerSimpson88354@users.noreply.github.com>
2026-08-16 20:46:04 -0400

    Update start.lua

--- start.lua
+++ start.lua
@@ -7,23 +7,6 @@ task.spawn(function()
 	end
 end)
 
-task.spawn(function()
-	local ok, err = pcall(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/main/aesthetic.lua"))()
-	end)
-	if not ok then
-		warn("Failed to load Aesthetic Sky:", err)
-	end
-end)
-
-task.spawn(function()
-	local ok, err = pcall(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/HomerSimpson88354/EternoSploitAssets/refs/heads/main/AestheticWater.lua"))()
-	end)
-	if not ok then
-		warn("Failed to load Aesthetic Water:", err)
-	end
-end)
 
 task.spawn(function()
 	local ok, err = pcall(function()
